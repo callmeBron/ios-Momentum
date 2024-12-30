6c64a8f697097e82d7bfc00e2dbf65869ad9a2a7
Bronwyn dos Santos <development.bronwyn@gmail.com>
2024-12-30 14:29:47 +0200

    removed filess

--- momentum/momentum/Onboarding(NeedsWork)/Models/OnboardingModel.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct OnboardingModel {
-    let imageName: String
-    let title: String
-    let description: String
-    let buttonText: String
-    let nextView: AnyView
-}
